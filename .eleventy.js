22cc1eecb29404c8916f6dc0cc6e91497ee55d32
Hugh Francis <hugh@sanctuary.computer>
2020-06-15 10:13:10 -0400

    First commit

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+const pluginSass = require("eleventy-plugin-sass");
+
+const sassPluginOptions = {
+};
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginSass, sassPluginOptions);
+  eleventyConfig.addPassthroughCopy("src/assets/images");
+
+  return {
+    dir: {
+      input: "src"
+    }
+  };
+};
