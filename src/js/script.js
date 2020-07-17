8b7bbf38b454cd82878880875ac2b5ef093049c6
Christine <43737723+chrismekim@users.noreply.github.com>
2020-07-17 10:18:38 -0400

    Add Nav (#9)
    * in progress
    
    * edit styling
    
    * clients in progress
    
    * in progress
    
    * remove script
    
    * adjust header image
    
    * styling changes
    
    * edit styling resources
    
    * update footer padding
    
    * adjust padding on buying header
    
    * update footer and script
    
    * fix footer img size
    
    * new image layouts in progress
    
    * in progress
    
    * edit resources
    
    * clients
    
    * QA
    
    * QA
    
    * QA
    
    * QA
    
    * in progress
    
    * in progress
    
    * in progress
    
    * in progress
    
    * add menu
    
    * add mobile styling
    
    * adjust padding
    
    * QA
    
    * nav adjustments
    
    * Q
    
    * in progress
    
    * add nav to subpages
    
    * in progress
    
    * in progress
    
    * in progress
    
    * test feature peek
    
    * test FP
    
    * test fp opacity
    
    * test fp opacity
    
    * test fp opacity
    
    * QA
    
    * QA
    
    * QA
    
    * update menu on subpages
    
    * merge conflicts
    
    * add AL
    
    * QA
    
    * in progress
    
    * fix typo
    
    * fix merge conflicts
    
    * update nav
    
    * QA
    
    * QA
    
    * QA

--- src/js/script.js
+++ src/js/script.js
@@ -30,4 +30,108 @@ const addActiveClassToFooterMenuLink = () => {
   };  
 };
 
-window.onload = addActiveClassToFooterMenuLink();
+
+//Nav
+const breakpointXL = 1280;
+const navMenuButton = document.getElementById('nav-menu-button');
+const navCloseButton = document.getElementById('nav-close-button');
+const menu = document.getElementById('menu');
+let menuIsActive = false;
+const studioCarbonNegativeButton = document.getElementById('studio-carbon-negative-button');
+const studioCarbonNegativeButtonContainer = document.getElementsByClassName('studio-carbon-negative-button-container')[0];
+const navMenuButtonContainer = document.getElementsByClassName('nav-menu--button-container')[0];
+const tableOfContentsTitle = document.getElementsByClassName('table-of-contents')[0];
+const view = document.getElementsByClassName('view')[0];
+const globe = document.getElementsByClassName('background-slide')[0];
+const menuLinks = document.getElementsByClassName('menu-link');
+
+const openMenu = () => {
+  menuIsActive = true;
+  tableOfContentsTitle.classList.remove('opacity-0');
+  tableOfContentsTitle.classList.add('opacity-1');
+  navMenuButton.classList.add('opacity-0', 'events-none', 'none');
+  navCloseButton.classList.remove('opacity-0', 'events-none', 'none');
+  navCloseButton.classList.add('opacity-1', 'events-all');
+  menu.classList.remove('menu--inactive');
+  menu.classList.add('menu--active');
+  view.classList.add('view--menu-is-active');
+
+  Array.from(menuLinks).forEach(link => {
+    link.classList.remove('menu-link--animation-fade-in-up-slow');
+    link.classList.add('menu-link--animation-fade-in-down-slow');
+  });
+
+  if (globe) {
+    globe.classList.add('globe--menu-is-active');
+  };
+
+  if (window.innerWidth < breakpointXL && menuIsActive) {
+    studioCarbonNegativeButton.classList.add('none');
+    studioCarbonNegativeButtonContainer.classList.remove('z-2000');
+  };
+
+  if (window.innerWidth > breakpointXL && menuIsActive) {
+    studioCarbonNegativeButton.classList.remove('none');
+    studioCarbonNegativeButtonContainer.classList.add('z-2000');
+  };
+};
+
+const closeMenu = () => {
+  menuIsActive = false;
+  tableOfContentsTitle.classList.add('opacity-0');
+  tableOfContentsTitle.classList.remove('opacity-1');
+  navCloseButton.classList.add('opacity-0', 'events-none', 'none');
+  navMenuButton.classList.add('opacity-1', 'events-all');
+  navMenuButton.classList.remove('opacity-0', 'events-none', 'none');
+  menu.classList.remove('menu--active');
+  menu.classList.add('menu--inactive');
+  view.classList.remove('view--menu-is-active');
+
+  Array.from(menuLinks).forEach(link => {
+    link.classList.remove('menu-link--animation-fade-in-down-slow');
+    link.classList.add('menu-link--animation-fade-in-up-slow');
+  });
+
+  if (globe) {
+    globe.classList.remove('globe--menu-is-active');
+    globe.classList.add('globe--menu-is-active');
+  };
+
+  if (window.innerWidth < breakpointXL && !menuIsActive) {
+    studioCarbonNegativeButton.classList.remove('none');
+    studioCarbonNegativeButtonContainer.classList.add('z-2000');
+  };
+};
+
+const handleResize = () => {
+  if (window.innerWidth < breakpointXL && menuIsActive) {
+    studioCarbonNegativeButton.classList.add('none');
+  };
+  
+  if (window.innerWidth > breakpointXL && menuIsActive) {
+    studioCarbonNegativeButton.classList.remove('none');
+    studioCarbonNegativeButtonContainer.classList.add('z-2000');
+
+  };
+};
+
+const handleScroll = () => {
+  const scrollTop = window.pageYOffset;
+
+  if (scrollTop > 400) {
+    studioCarbonNegativeButton.classList.remove('studio-carbon-negative-button--inactive');
+    studioCarbonNegativeButton.classList.add('studio-carbon-negative-button--active');
+  };
+
+  if (scrollTop < 400) {
+    studioCarbonNegativeButton.classList.remove('studio-carbon-negative-button--active');
+    studioCarbonNegativeButton.classList.add('studio-carbon-negative-button--inactive');
+  };
+
+}
+
+addActiveClassToFooterMenuLink();
+navMenuButton.addEventListener('click', openMenu);
+navCloseButton.addEventListener('click', closeMenu);
+window.addEventListener('scroll', handleScroll);
+window.addEventListener('resize', handleResize);
