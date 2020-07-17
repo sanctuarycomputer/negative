a6dfaa50157f9165937fdd1733ca7570514bba66
Christine Kim <chrismekim@gmail.com>
2020-07-17 12:04:21 -0500

    in progress

--- src/js/script.js
+++ src/js/script.js
@@ -130,6 +130,19 @@ const handleScroll = () => {
 
 }
 
+// Add transition when section enters viewport
+const sections = document.querySelectorAll('section.fade-in-up');
+
+window.onscroll = function() {
+  if (!document.querySelectorAll('section.fade-in-up:not(.visible)')) return;
+
+  for (const section of sections) {
+    if (section.getBoundingClientRect().top <= window.innerHeight * 0.75 && section.getBoundingClientRect().top > 0) {
+      section.classList.add('visible');
+    }
+  }
+};
+
 addActiveClassToFooterMenuLink();
 navMenuButton.addEventListener('click', openMenu);
 navCloseButton.addEventListener('click', closeMenu);
